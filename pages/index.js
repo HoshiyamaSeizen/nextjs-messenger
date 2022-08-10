8ac278b8b8d86b8b0e1b991357a4922edf093196
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2022-08-10 14:48:12 +0300

    nextjs boilerplate

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+	return (
+		<div>
+			<Head>
+				<title>Messenger</title>
+				<meta name="description" content="Messenger created with NextJS" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+		</div>
+	);
+};
+
+export default Home;
